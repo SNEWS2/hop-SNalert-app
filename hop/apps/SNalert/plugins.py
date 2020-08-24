c572c54f257ac1c453885d98f57566cb36ad4a0f
yiyangxu1998 <yx48@rice.edu>
2020-08-24 14:43:39 -0500

    add alert message to plugins

--- hop/apps/SNalert/plugins.py
+++ hop/apps/SNalert/plugins.py
@@ -1,10 +1,12 @@
 from hop import plugins
 from .dataPacket.observationMsg import ObservationMsg
 from .dataPacket.heartbeatMsg import HeartbeatMsg
+from .dataPacket.alertMsg import AlertMsg
 
 @plugins.register
 def get_models():
     return {
         "observationMsg": ObservationMsg,
-        "heartbeatMsg": HeartbeatMsg
+        "heartbeatMsg": HeartbeatMsg,
+        "alertMsg": AlertMsg
     }

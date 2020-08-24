5ae5fcb534f7f2d0804a0b571f89269053ec55e6
Patrick Godwin <patrick.godwin@psu.edu>
2020-08-24 18:04:56 -0400

    add SNEWSBase message class for reuse in other SNEWS messages, rename to preserve uniqueness across hop

--- hop/apps/SNalert/dataPacket/observationMsg.py
+++ hop/apps/SNalert/dataPacket/observationMsg.py
@@ -1,85 +1,21 @@
 from dataclasses import dataclass
-import json
 
-# import hop.models
-from hop.models import MessageModel
-from dataclasses_jsonschema import JsonSchemaMixin
-from .dataPacketID import DataPacketID
+from . import SNEWSBase
+
 
 @dataclass
-class ObservationMsg(MessageModel, JsonSchemaMixin):
+class SNEWSObservation(SNEWSBase):
     """
     Defines an observation published by a detector.
 
     Formatted as a dictionary with the schema defined in the jsonSchema file.
 
     """
-    # header: dict
-    # body: str
-    message_id: str
     detector_id: str
     sent_time: str
     neutrino_time: str
     machine_time: str
     location: str
-    p_value: str
+    p_value: float
     status: str
     content: str
-
-    @staticmethod
-    def getID():
-        # return DataPacketID(ObservationMsg)
-        return DataPacketID("ObservationMsg")
-
-    def __str__(self):
-        return [(attribute.upper() + ":").ljust(9) + value for attribute, value in self.__dict__.items()]
-
-    @classmethod
-    def load(cls, input):
-        """
-
-        :param input: A serialized json string converted by asdict().
-        :return:
-        """
-
-        # detector_name = input
-        dict = json.loads(input)
-        return cls(
-            message_id=dict['message_id'],
-            detector_id=dict['detector_id'],
-            sent_time=dict['sent_time'],
-            neutrino_time=dict['neutrino_time'],
-            machine_time=dict['machine_time'],
-            location=dict['location'],
-            p_value=dict['p_value'],
-            status=dict['status'],
-            content=dict['content']
-        )
-
-    def getMessageID(self):
-        return self.message_id
-
-    def getDetectorID(self):
-        return self.detector_id
-
-    def getSentTime(self):
-        return self.sent_time
-
-    def getNeutrinoTime(self):
-        return self.neutrino_time
-
-    def getMachineTime(self):
-        return self.machine_time
-
-    def getLocation(self):
-        return self.location
-
-    def getPValue(self):
-        return self.p_value
-
-    def getStatus(self):
-        return self.status
-
-    def getMsgContent(self):
-        return self.content
-

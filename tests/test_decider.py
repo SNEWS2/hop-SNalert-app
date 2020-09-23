39f6b6d36d00ae17061a01968338770fa68376d4
Patrick Godwin <patrick.godwin@psu.edu>
2020-09-23 12:41:22 -0400

    add minimal decider tests

---
+++ tests/test_decider.py
@@ -0,0 +1,23 @@
+from unittest.mock import patch, MagicMock
+
+import mongomock
+
+from snews import decider
+
+
+@mongomock.patch(servers=(('localhost', 27017),))
+def test_decider(mongodb):
+    decide = decider.Decider(
+        coinc_threshold=10,
+        msg_expiration=120,
+        datetime_format="%y/%m/%d %H:%M:%S",
+        mongo_server="mongodb://localhost:27017/",
+        drop_db=False,
+    )
+    with patch.object(decide.db, 'cache', mongodb.cache):
+        # check messages in cache
+        messages = list(decide.getCacheMessages())
+        assert mongodb.cache.count() == len(messages)
+
+        # check deciding functionality, should determine coincidence
+        assert decide.deciding()

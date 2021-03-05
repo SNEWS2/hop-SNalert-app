78766104069a305b2ae7d9f279f6ea04220618d6
bfc5288 <bfc5288@psu.edu>
2021-03-05 17:40:58 -0500

    Changed topic startup messages to reflect changes to the BLOB format in hop-client v0.3. The new messages are backwards compatible with v0.2

--- run-containers.sh
+++ run-containers.sh
@@ -5,7 +5,7 @@ echo "## The SCiMMA Server container is loading, please wait..."
 sleep 20
 echo "## The SCiMMA Server container has loaded. Generating the ${OBSERVATION_TOPIC} and ${ALERT_TOPIC} topics..."
 
-echo "starting up the ${OBSERVATION_TOPIC} topic" | hop publish -f BLOB $OBSERVATION_TOPIC --no-auth
-echo "starting up the ${ALERT_TOPIC} topic" | hop publish -f BLOB $ALERT_TOPIC --no-auth
+echo '{"content": "initial message"}' | hop publish -f BLOB $OBSERVATION_TOPIC --no-auth
+echo '{"content": "initial message"}' | hop publish -f BLOB $ALERT_TOPIC --no-auth
 
 echo "## The ${OBSERVATION_TOPIC} and ${ALERT_TOPIC} topics have been loaded."

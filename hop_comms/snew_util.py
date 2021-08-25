532941132af472df4be9d80205c8b75f1354542d
storreslara <sebastiantorreslara17@gmail.com>
2021-08-25 14:28:21 -0500

    added env file and fix some errors in the hop_comms modules

---
+++ hop_comms/snew_util.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+
+'''
+loads SNEWS env 
+'''
+def set_env(env_path):
+    load_dotenv(env_path)
+
+'''
+returns detector name (snews format)
+'''
+def get_detector_id(num):
+    detector_id = {
+        1: "Super-K",
+        2: "Hyper-K",
+        3: "SNO+",
+        4: "KamLAND",
+        5: "LVD",
+        6: "ICE",
+        7: "Borexino",
+        8: "HALO-1kT",
+        9: "HALO",
+        10: "NOvA",
+        11: "KM3NeT",
+        12: "Baksan",
+        13: "JUNO",
+        14: "LZ",
+        15: "DUNE",
+        16: "MicroBooNe",
+        17: "SBND",
+        18: "DS-20K",
+        19: "XENONnT",
+        20: "PandaX-4T",
+    }
+    return detector_id(num)

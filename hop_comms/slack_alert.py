53c69aff97307e808a91a9269a9c971cab7946f0
storreslara <sebastiantorreslara17@gmail.com>
2021-08-31 08:08:52 -0500

    Updated slack_alert (current auth key is invalid) and added snews_db.py (WIP)

--- hop_comms/slack_alert.py
+++ hop_comms/slack_alert.py
@@ -1,16 +1,28 @@
 # Author: Sebastian Torres-Lara, Univ of Houston
+# imports
+from slack_sdk import WebClient
 
-# Imports
-import json
-import requests
 
-# this method creates a slack request to a test channel (webhook_url)
-# Sends either and aler or obs notification (depending on the message state)
-def send_slack_msg(topic, message):
-    webhook_url = "https://hooks.slack.com/services/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"
-    if topic == 'A':
-        slack_data = {'text': f"ALERT from: {message['detector_id']}:\n{str(message)}"}
-        requests.post(webhook_url, data=json.dumps(slack_data), headers={'Content_type': 'application/json'})
-    elif topic == 'O':
-        slack_data = {'text': f"OBSERVATION from: {message['detector_id']}:\n{str(message)}"}
-        requests.post(webhook_url, data=json.dumps(slack_data), headers={'Content_type': 'application/json'})
+# from slack_sdk.errors import SlackApiError
+
+#  make sure you have the slackAPI installed pip install slack_sdk
+
+# this method makes the snews msgs easier to read
+def readable_msgs(dic):
+    msg = ''
+    for key in dic:
+        msg += f'{key} --> {dic[key]} \n'
+    return msg
+
+
+# this method send a slack message to SNEWS_ALERTS-general
+def send_slack_msg(message_type, snews_message):
+    # verification token for slack app and channel id
+    slack_token = 'xoxb-2120915545714-2134186008769-efonv0EKfryvAH7JNde6IrMm'
+    slack_channel_id = 'C02344AHKHV'
+    # set up client and give it the slack token
+    client = WebClient(token=slack_token)
+    # format message
+    slack_message = f"Message from {snews_message['detector_id']}\nType: {message_type}\n{readable_msgs(snews_message)}\n\n"
+    # pass message to WebClient and send it to the slack channel
+    client.chat_postMessage(channel=slack_channel_id, text=slack_message)
